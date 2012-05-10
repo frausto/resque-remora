bb928cacb81cae84189847485a1dd9645ecb3c1b
frausto <nrfrausto@gmail.com>
2012-05-09 21:30:24 -0400

    get specs passing for rubinius 18 mode

--- spec/resque/plugins/remora/push_pop_spec.rb
+++ spec/resque/plugins/remora/push_pop_spec.rb
@@ -10,22 +10,22 @@ describe Resque::Plugins::Remora::PushPop do
   
   describe "#push" do
     it "should add the remora information to the end of the job" do
-      TestJob.stub(:remora_attachment => {:remora => {:time => @now, :olah => "oh hai!"}})
+      TestJob.stub(:remora_attachment => {:remora => {:time => @now.to_i, :olah => "oh hai!"}})
       Resque.enqueue(TestJob, "arg1")
-      Resque.redis.lindex("queue:test",0).should =~ /^\{.*\"remora\":(\{\"olah\":\"oh hai!\",\"time\":\"#{@now}\"\}||\{\"time\":\"#{@now}\",\"olah\":\"oh hai!\"\})/
+      Resque.redis.lindex("queue:test",0).should =~ /^\{.*\"remora\":(\{\"olah\":\"oh hai!\",\"time\":\"#{@now.to_i}\"\}||\{\"time\":\"#{@now.to_i}\",\"olah\":\"oh hai!\"\})/
     end
   end
   
   describe "#pop" do
     before do
-      @attachment = {:remora => {:time => @now, :olah => "oh hai!"}}
-      @pop_result = {"args"=>["arg1"], "class"=>"TestJob", "remora"=>{"time"=>@now.to_s, "olah"=>"oh hai!"}}
+      @attachment = {:remora => {:time => @now.to_i, :olah => "oh hai!"}}
+      @pop_result = {"args"=>["arg1"], "class"=>"TestJob", "remora"=>{"time"=>@now.to_i, "olah"=>"oh hai!"}}
     end
     
     it "should get the remora information for processing" do
       TestJob.stub(:remora_attachment => @attachment)
       Resque.enqueue(TestJob, "arg1")
-      TestJob.should_receive(:process_remora).with("test", {'time' => @now.to_s, 'olah' => "oh hai!"})
+      TestJob.should_receive(:process_remora).with("test", {'time' => @now.to_i, 'olah' => "oh hai!"})
       Resque.pop("test").should == @pop_result
     end
     
